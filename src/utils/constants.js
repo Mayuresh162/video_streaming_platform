3c3de851051a4912cc0b8e476e9adb43b6e37be4
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2023-02-23 18:54:49 +0530

    Add files via upload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ const GOOGLE_API_KEY = "AIzaSyC5Fe_rhSZ7TnGPTHycztTh-7AkOiEX4DM";
 export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
